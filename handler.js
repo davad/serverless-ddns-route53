02186f8a1fe319f2e889789eb86d24769ce0c18f
David Landry <david@dmwl.net>
2017-10-04 15:53:02 -0400

    Add shared secret for authorizing domain update. Limit access to a single zone.

--- handler.js
+++ handler.js
@@ -4,19 +4,32 @@ var AWS = require("aws-sdk");
 var route53 = new AWS.Route53();
 
 module.exports.update = (event, context, cb) => {
-  if (!event.query.hosted_zone_id) { cb('Missing hosted_zone_id param'); return false; }
-  if (!event.query.name) { cb('Missing name param'); return false; }
+  if (!event.headers.Authorization || event.headers.Authorization != process.env.SHARED_SECRET) {
+    cb(null,  { statusCode: 400, body: 'Missing or invalid authorization token'});
+    return false; 
+  }
+  if (!event.queryStringParameters.hosted_zone_id) { cb(null,  { statusCode: 400, body: 'Missing hosted_zone_id param'}); return false; }
+  if (!event.queryStringParameters.name) { cb(null,  { statusCode: 400, body:'Missing name param'}); return false; }
+
+  /*
+  options = {
+    hosted_zone_id: event.queryStringParameters.hosted_zone_id,
+    name: event.queryStringParameters.name
+  }
+ */
+
+  hashed
 
   route53.changeResourceRecordSets({
-    HostedZoneId: event.query.hosted_zone_id,
+    HostedZoneId: event.queryStringParameters.hosted_zone_id,
     ChangeBatch: {
       Changes: [
         {
           Action: 'UPSERT',
           ResourceRecordSet: {
-            Name: event.query.name,
+            Name: event.queryStringParameters.name,
             Type: 'A',
-            ResourceRecords: [ { Value: event.identity.sourceIp } ],
+            ResourceRecords: [ { Value: event.requestContext.identity.sourceIp } ],
             TTL: 300
           }
         }
@@ -26,9 +39,9 @@ module.exports.update = (event, context, cb) => {
   }, function (err, data) {
     if (err) {
       console.log(err, err.stack);
-      cb('Failed to update record set: ' + JSON.stringify({ message: 'FAIL', err: err.stack }))
+      cb(null, { statusCode: 500, body: JSON.stringify({ message: 'FAIL', err: err.stack })})
     } else {
-      cb(null, { message: 'OK' })
+      cb(null, { body: JSON.stringify({message: 'OK' })})
     }
   });
 }
