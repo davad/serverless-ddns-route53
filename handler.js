6ac47a1f96db529b5c90d20f6f385f432b674562
David Landry <david@dmwl.net>
2017-10-13 15:49:33 -0400

    Move from env variables to DynamoDB
    * Authenticate based on items in a DynamoDB table
    * Store hosted zone id in table

--- handler.js
+++ handler.js
@@ -1,48 +1,127 @@
 'use strict';
 
+const dynamodb = require('./dynamodb');
+
 var AWS = require("aws-sdk");
 var route53 = new AWS.Route53();
 
-module.exports.update = (event, context, cb) => {
-  if (!event.headers.Authorization || event.headers.Authorization != process.env.SHARED_SECRET) {
+function validInput (event,context, cb) {
+  if (!event.headers.Authorization) {
     cb(null,  { statusCode: 400, body: 'Missing or invalid authorization token'});
     return false; 
   }
-  if (!event.queryStringParameters.hosted_zone_id) { cb(null,  { statusCode: 400, body: 'Missing hosted_zone_id param'}); return false; }
   if (!event.queryStringParameters.name) { cb(null,  { statusCode: 400, body:'Missing name param'}); return false; }
+  return true
+}
 
-  /*
-  options = {
-    hosted_zone_id: event.queryStringParameters.hosted_zone_id,
-    name: event.queryStringParameters.name
-  }
+function validAuth (payload, cb) {
+  // Authenticate client
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      fqdn: payload.name,
+      client_id: payload.id,
+    },
+  };
 
-  hashed
 
- */
 
-  route53.changeResourceRecordSets({
-    HostedZoneId: event.queryStringParameters.hosted_zone_id,
-    ChangeBatch: {
-      Changes: [
-        {
+ return dynamodb.get(params, (error, result) => {
+    // handle potential errors
+    var valid = false;
+    if (error) {
+      console.error(error);
+      valid = false;
+    }
+
+    if(result.hasOwnProperty("Item")
+       && result.Item.hasOwnProperty("api_keys")
+       && result.Item.api_keys.includes(payload.key)) {
+
+      valid = true;
+    }
+
+    if(!valid) {
+      cb(null, {
+        statusCode: 403,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t authenticate the request.',
+      });
+    }
+
+    return valid;
+
+  });
+}
+
+function updateRecord(payload, cb) {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      fqdn: payload.name,
+      client_id: payload.id,
+    },
+  };
+
+  dynamodb.get(params, (error, result) => {
+    var valid = false;
+    if (error) {
+      console.error(error);
+    }
+
+    var hosted_zone_id = '';
+    if (result.hasOwnProperty("Item") 
+        && result.Item.hasOwnProperty("hosted_zone_id")){
+        hosted_zone_id = result.Item.hosted_zone_id;
+    }
+
+    if (hosted_zone_id == '') {
+      cb(null, { statusCode: 500, body: JSON.stringify({ message: 'FAIL', error: error })})
+      return
+    }
+
+    route53.changeResourceRecordSets({
+      HostedZoneId: hosted_zone_id,
+      ChangeBatch: {
+        Changes: [
+          {
           Action: 'UPSERT',
           ResourceRecordSet: {
-            Name: event.queryStringParameters.name,
+            Name: payload.name,
             Type: 'A',
-            ResourceRecords: [ { Value: event.requestContext.identity.sourceIp } ],
+            ResourceRecords: [ { Value: payload.ip } ],
             TTL: 300
           }
         }
-      ],
-      Comment: 'ddns update'
-    }
-  }, function (err, data) {
-    if (err) {
-      console.log(err, err.stack);
-      cb(null, { statusCode: 500, body: JSON.stringify({ message: 'FAIL', err: err.stack })})
-    } else {
-      cb(null, { body: JSON.stringify({message: 'OK' })})
-    }
+        ],
+        Comment: 'ddns update'
+      }
+    }, function (err, data) {
+      if (err) {
+        console.error(err, err.stack);
+        cb(null, { statusCode: 500, body: JSON.stringify({ message: 'FAIL', err: err.stack })})
+      } else {
+        cb(null, { body: JSON.stringify({message: 'OK' })})
+      }
+    });
   });
+}
+
+module.exports.update = (event, context, cb) => {
+  var authorization = event.headers.Authorization.split(":");
+
+  var payload = {
+    auth: event.headers.Authorization,
+    id: authorization[0],
+    key: authorization[1],
+    name: event.queryStringParameters.name,
+    ip: event.requestContext.identity.sourceIp
+  };
+
+  // Validate input and authenticate client
+  if (!validInput(event, context, cb)
+    || !validAuth(payload, cb)) { return false; }
+
+  updateRecord(payload, cb);
+
 }
